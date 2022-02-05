dfdbe0797a8f4e7dc1ac8bcd54c37fada330d84b
REDOY <74199587+555REDOY555@users.noreply.github.com>
2022-02-05 14:47:22 +0600

    Frist Commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAcHpq_wlPjKHVjq0s9awiESPM9WEWiqjU",
+    authDomain: "riders-4adb6.firebaseapp.com",
+    projectId: "riders-4adb6",
+    storageBucket: "riders-4adb6.appspot.com",
+    messagingSenderId: "430944047020",
+    appId: "1:430944047020:web:a507ab5ff88defb6a0f409"
+};
+
+export default firebaseConfig;
